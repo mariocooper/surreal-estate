fd47a538b8447376705b0abb8cafd1fc0d106d70
Mario Cooper <mariocooper00@hotmail.com>
2023-12-08 19:15:36 +0000

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import '../styles/app.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        Surreal Estate
+      </header>
+    </div>
+  );
+}
+
+export default App;
